463603ca5f850e992f2da4e0adc303bdfa5ba7f8
Ben Cho <ben.cho@gmail.com>
2024-10-06 11:34:10 -0600

    added pypi workflow

---
+++ README.md
@@ -0,0 +1,30 @@
+# GGPLOTLY
+
+A data visualization library for Python that combines the power of ggplot and plotly.
+
+## Installation
+
+```bash
+pip install ggplotly
+```
+
+## Usage
+
+```python
+from ggplotly import *
+import pandas as pd
+
+df = pd.DataFrame({
+    'x': [1, 2, 3, 4, 5],
+    'y': [1, 2, 3, 4, 5]
+})
+
+ggplot(df, aes(x='x', y='y')) + geom_point()
+```
+
+## Working Geoms
+
+- geom_point
+- geom_line
+- geom_bar
+- geom_area
--- setup.py
+++ setup.py
@@ -13,4 +13,17 @@ setup(
     ],
     author="Ben Cho",
     description="An advanced ggplot2-like plotting system for Python built on top of Plotly",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bbcho/ggplotly",  # Add your GitHub URL here
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    python_requires=">=3.6",
 )
