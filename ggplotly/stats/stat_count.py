daa5d20ba6f7209976aeef32bddc8a70db1f2793
Ben Cho <ben.cho@gmail.com>
2024-10-12 18:13:55 -0600

    geom_bar working - exclude stat_bin for now

--- ggplotly/stats/stat_count.py
+++ ggplotly/stats/stat_count.py
@@ -12,15 +12,18 @@ class stat_count(Stat):
         self.aggregator = "count"
 
     def compute(self, data):
+
         data = data.copy()
         stat = self.aggregator
+        print(stat)
 
         grouping = list(set([v for k, v in self.mapping.items()]))
         grouping = [g for g in grouping if g in data.columns]
         grouping_keys = list(set([k for k, v in self.mapping.items()]))
 
         # if x XOR y in grouping
-        if ("x" in grouping_keys) ^ ("y" in grouping_keys):
+        # if ("x" in grouping_keys) ^ ("y" in grouping_keys):
+        if len(data[grouping]) == 1:
 
             # if len(data.columns)  == 1:
             tf = data.value_counts()
